fdd9d5d9a46b09e92831a99f0e15f67f66ac8b5f
Igor <igor.ptz@hotmail.com>
2021-07-28 22:16:43 -0300

    Update

--- src/manipulacao/de/datas/UsoDoJavaTime.java
+++ src/manipulacao/de/datas/UsoDoJavaTime.java
@@ -20,5 +20,57 @@ public class UsoDoJavaTime {
 		LocalDateTime dataHoraAtual = LocalDateTime.now();
 		
 		System.out.println("Data hora atual: " + dataHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
-	}
+		
+		System.out.println();
+		
+		LocalDate dataNova = LocalDate.parse("2021-07-19");
+		
+		System.out.println("Data nova: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.plusDays(5);
+		
+		System.out.println("Data nova com mais 5 dias: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));	
+		
+		dataNova = dataNova.plusWeeks(5);
+		
+		System.out.println("Data nova com mais 5 semanas: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.plusMonths(5);
+		
+		System.out.println("Data nova com mais 5 meses: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.plusYears(5);
+		
+		System.out.println("Data nova com mais 5 anos: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))); 
+	
+		System.out.println();
+		
+		System.out.println("Data nova: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.minusDays(5);
+		
+		System.out.println("Data nova com menos 5 dias: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.minusWeeks(5);
+		
+		System.out.println("Data nova com menos 5 semanas: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.minusMonths(5);
+		
+		System.out.println("Data nova com menos 5 meses: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		dataNova = dataNova.minusYears(5);
+		
+		System.out.println("Data nova com menos 5 anos: " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println();
+		
+		// Simulando criação de parcelas
+		for(int parcela = 1; parcela <= 12; parcela++) {
+			
+			dataNova = dataNova.plusMonths(1);
+			
+			System.out.println("Parcela nº " + parcela + " com vencimento em " + dataNova.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		}
+	} 
 }
